8dccc325e4908b42d90f0b1555996e41cb9f96ad
Tan530 <126578225+Tan530@users.noreply.github.com>
2023-03-14 15:25:20 +0800

    update
    this is test

--- index.php
+++ index.php
@@ -10,7 +10,10 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
     </head>
     <body>
         <?php
+        
         <p>Test change something</p>
+            <br/>
         ?>
+        <h1>test</h1>
     </body>
 </html>
